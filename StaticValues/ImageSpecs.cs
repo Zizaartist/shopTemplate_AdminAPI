787cee74c47e9ed42d82330c5d30e3bab125e8ef
Ренат Зиазетдинов <zizaartist@gmail.com>
2021-05-18 16:35:25 +0900

    Fixed an issue with images

--- StaticValues/ImageSpecs.cs
+++ StaticValues/ImageSpecs.cs
@@ -7,15 +7,13 @@ namespace ApiClick.StaticValues
 {
     public class ImageSpecs
     {
-        public const int AD_BANNER_MAX = 700;
         public const int CATEGORY_IMAGE_MAX = 400;
         public const int PRODUCT_IMAGE_MAX = 400;
 
         public enum ImageType 
         {
-            adBanner,
-            categoryImage,
-            productImage
+            categoryImage = 2,
+            productImage = 3
         }
         
         /// <summary>
@@ -23,7 +21,6 @@ namespace ApiClick.StaticValues
         /// </summary>
         public static Dictionary<ImageType, int> ImageTypeToMax = new Dictionary<ImageType, int>() 
         {
-            { ImageType.adBanner, AD_BANNER_MAX },
             { ImageType.categoryImage, CATEGORY_IMAGE_MAX },
             { ImageType.productImage, PRODUCT_IMAGE_MAX }
         };
