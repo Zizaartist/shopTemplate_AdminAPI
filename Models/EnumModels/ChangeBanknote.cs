e202efb012d7aafa2ce381f0f17573c872aada81
keskilVladimirov <43175524+keskilVladimirov@users.noreply.github.com>
2021-05-20 13:12:39 +0900

    ChangeBanknote

---
+++ Models/EnumModels/ChangeBanknote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopAdminAPI.Models.EnumModels
+{
+    public enum ChangeBanknote
+    {
+        fiveHundred = 500,
+        thousand = 1000,
+        twoThousand = 2000,
+        fiveThousand = 5000
+    }
+}
